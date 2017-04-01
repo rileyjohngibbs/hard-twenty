4cbbe5c41d7f6723a31a34ed0ef230c8f0348a9b
Riley Gibbs <riley@citizennet.com>
2017-03-31 22:21:31 -0700

    Clean up commented code
    Don't need these old test examples. Pulling out into another file is cleaner. Better.

--- compose_and_test/compose.py
+++ compose_and_test/compose.py
@@ -1,6 +1,3 @@
-# import unittest
-# from test import test_support
-
 def compose(*funcs):
 	if len(funcs) == 1:
 		def composition(*args, **kwargs):
@@ -13,19 +10,3 @@ def compose(*funcs):
 def add(x): return x+1
 def mult(x): return x*2
 def square(x): return x**2
-
-'''bar = compose(add, mult, square)
-
-print bar(4) # Should print 100 to screen
-
-class MyTestCase1(unittest.TestCase):
-
-	def test_foo(self):
-		foo = compose(add, mult, square)
-		assert foo(4) == 10, "Fuuuuuck"
-
-	def test_bar(self):
-		bar = compose(add, add, square, mult)
-		assert bar(1) == 10, "Triple fuuuuuuck"
-
-test_support.run_unittest(MyTestCase1)'''
