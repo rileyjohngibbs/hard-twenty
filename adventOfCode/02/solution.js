cb46a2f62268ea80e1696abe9e35edcfd1dc8eb3
Riley Gibbs <rileyjohngibbs@gmail.com>
2018-12-05 12:10:24 -0600

    Added JavaScript solution to Day Two, Part Two

--- adventOfCode/02/solution.js
+++ adventOfCode/02/solution.js
@@ -2,6 +2,8 @@ var fs = require('fs');
 
 lines = fs.readFileSync('input.txt', 'ascii').split('\n');
 
+// Part One
+
 function count(line) {
   counts = [];
   line.split('').forEach((c) => {
@@ -29,3 +31,29 @@ lines.forEach((line) => {
 });
 
 console.log(twice_counts * thrice_counts);
+
+// Part Two
+
+function samesies(base, comparison) {
+  var same_count = samechars(base, comparison).length;
+  var differences = base.length - same_count;
+  return differences === 1;
+}
+
+function samechars(base, comparison) {
+  var matches = "";
+  base.split("").forEach((c, i) => {
+    if (c === comparison[i]) {
+      matches += c;
+    }
+  });
+  return matches;
+}
+
+lines.forEach((base_line, i) => {
+  lines.slice(i + 1).forEach((comparison_line) => {
+    if (samesies(base_line, comparison_line)) {
+      console.log(samechars(base_line, comparison_line));
+    }
+  });
+});
