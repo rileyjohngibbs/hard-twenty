7af33bea7b686be7adaf375d841cf83c383e2824
Riley Gibbs <rileyjohngibbs@gmail.com>
2018-12-05 10:16:06 -0600

    Finished Part One of Day Three of Advent of Code

---
+++ adventOfCode/03/solution.py
@@ -0,0 +1,34 @@
+import numpy as np
+import re
+
+
+with open("input.txt", "r") as inputfile:
+    lines = [line.replace("\n", "") for line in inputfile.readlines()]
+
+
+def parse_line(line):
+    pattern = r"#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)"
+    match = re.match(pattern, line)
+    if not match:
+        raise Exception(f"No pattern match: {line}")
+    groups = match.groups()
+    return dict(zip(
+        ["id", "x", "y", "width", "height"],
+        map(int, groups)
+    ))
+
+
+parsed_lines = list(map(parse_line, lines))
+shape = (
+    max(line["y"] + line["height"] + 1 for line in parsed_lines),
+    max(line["x"] + line["width"] + 1 for line in parsed_lines)
+)
+fabric = np.zeros(shape, int)
+
+for line in parsed_lines:
+    fabric[
+        line["y"]:line["y"] + line["height"],
+        line["x"]:line["x"] + line["width"]
+    ] += 1
+
+print(len(np.transpose((fabric > 1).nonzero())))
