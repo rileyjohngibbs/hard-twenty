369732a96bad74f1758991414f4391f380e23c08
Riley Gibbs <rileyjohngibbs@gmail.com>
2018-12-06 21:41:07 -0600

    Added Day Five, Part Two solution in JavaScript

--- adventOfCode/05/solution.js
+++ adventOfCode/05/solution.js
@@ -2,16 +2,16 @@ var fs = require('fs');
 
 const polymer = fs.readFileSync('input.txt', 'ascii').replace('\n', '');
 
-function collapse(polymer) {
+function collapse(polymer, drop) {
   let final_polymer = [];
-  polymer.split('').forEach((a) => {
+  polymer.replace(new RegExp(drop, 'gi'), '').split('').forEach((a) => {
     final_polymer.push(a);
     while (reacts(...final_polymer.slice(a.length - 3))) {
       final_polymer.pop();
       final_polymer.pop();
     }
   });
-  return final_polymer;
+  return final_polymer.join('');
 }
 
 function reacts(a, b) {
@@ -19,5 +19,21 @@ function reacts(a, b) {
   return (a != b) && (a.toLowerCase() === b.toLowerCase());
 }
 
-const collapsed = collapse(polymer).join('');
+
+// Part One
+
+
+const collapsed = collapse(polymer);
 console.log(collapsed.length);
+
+
+// Part Two
+
+
+const alphabet = [...Array(26).keys()]
+  .map((x) => String.fromCharCode(x + 97));
+console.log(
+  Math.min(...alphabet.map(
+    (a) => collapse(collapsed, a).length
+  ))
+);
