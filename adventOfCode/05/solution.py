5e29e468b742f7646480e0b1b0cf3ea741944d97
Riley Gibbs <rileyjohngibbs@gmail.com>
2018-12-06 20:48:15 -0600

    Refactored Day Five, Part One and solved Part Two in Python

--- adventOfCode/05/solution.py
+++ adventOfCode/05/solution.py
@@ -2,17 +2,29 @@ with open('input.txt', 'r') as inputfile:
     polymer = inputfile.read().replace('\n', '')
 
 
+def collapse(polymer, drop=''):
+    final_polymer = []
+    for unit in polymer.replace(drop.lower(), '').replace(drop.upper(), ''):
+        final_polymer.append(unit)
+        last_two = final_polymer[-2:]
+        while len(final_polymer) >= 2 and react(*last_two):
+            final_polymer[-2:] = []
+            last_two = final_polymer[-2:]
+    return ''.join(final_polymer)
+
+
 def react(a, b):
     return a != b and a.lower() == b.lower()
 
 
-final_polymer = []
+# Part One
 
-for unit in polymer:
-    final_polymer.append(unit)
-    last_two = final_polymer[-2:]
-    while len(final_polymer) >= 2 and react(*last_two):
-        final_polymer[-2:] = []
-        last_two = final_polymer[-2:]
 
-print(len(final_polymer))
+collapsed = collapse(polymer)
+print(len(collapsed))
+
+
+# Part Two
+
+alphabet = [chr(i) for i in range(65, 65+26)]
+print(min(len(collapse(collapsed, alpha)) for alpha in alphabet))
