d82ec174e742ea76bb68016b25f9f7fd70022e57
Riley Gibbs <rileyjohngibbs@gmail.com>
2018-12-10 12:18:12 -0800

    Added Day One, Part Two solution in Python

--- adventOfCode/01/solution.py
+++ adventOfCode/01/solution.py
@@ -1,4 +1,29 @@
 with open("input.txt", "rb") as inputfile:
     lines = inputfile.readlines()
 
+
+# Part One
+
+
 print(sum(map(int, lines)))
+
+
+# Part Two
+
+
+frequencies = set([0])
+current_frequency = 0
+
+def linegen():
+    while True:
+        for line in lines:
+            yield line
+
+for line in linegen():
+    current_frequency += int(line)
+    if current_frequency in frequencies:
+        break
+    else:
+        frequencies.add(current_frequency)
+
+print(current_frequency)
