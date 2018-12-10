571206535c046793f27be732971a8dd2a2cdf7fe
Riley Gibbs <rileyjohngibbs@gmail.com>
2018-12-10 12:25:05 -0800

    Refactored Day One, Part One in JavaScript

--- adventOfCode/01/solution.js
+++ adventOfCode/01/solution.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 
-fs.readFile('input.txt', 'ascii', function(err, contents) {
-  var lines = contents.split('\n');
-  var total = lines.map(function(x) {
-    return parseInt(x) || 0;
-  }).reduce(function(x, y) {
-    return x + y;
-  });
-  console.log(total);
-});
+const lines = fs.readFileSync('input.txt', 'ascii')
+  .split('\n')
+  .map((x) => parseInt(x.replace('\n')))
+  .filter((x) => x);
+
+// Part One
+
+const total = lines.reduce((x, y) => x + y);
+
+console.log(total);
+
+// Part Two
+
+
