1bba621dded2ffe04ee15a02e1078792dd6fa458
Riley Gibbs <rileyjohngibbs@gmail.com>
2018-12-11 12:44:08 -0800

    Finished Day Seven, Step Two solution in Python

--- adventOfCode/07/solution.py
+++ adventOfCode/07/solution.py
@@ -1,10 +1,24 @@
 import numpy as np
 import re
+import sys
 
-with open('input.txt', 'r') as inputfile:
-    input_lines = [s.replace('\n', '') for s in inputfile.readlines()]
+TESTING = "test" in sys.argv
 
-steps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+if TESTING:
+    input_lines = [
+        "Step C must be finished before step A can begin.",
+        "Step C must be finished before step F can begin.",
+        "Step A must be finished before step B can begin.",
+        "Step A must be finished before step D can begin.",
+        "Step B must be finished before step E can begin.",
+        "Step D must be finished before step E can begin.",
+        "Step F must be finished before step E can begin.",
+    ]
+else:
+    with open('input.txt', 'r') as inputfile:
+        input_lines = [s.replace('\n', '') for s in inputfile.readlines()]
+
+steps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' if not TESTING else 'ABCDEF'
 
 def index(step):
     return ord(step) - 65
@@ -38,18 +52,21 @@ print(''.join(order))
 # Part Two
 
 
-requirements = np.zeros((26, 26), dtype=int)
+requirements = np.zeros((27 - 20*TESTING, 26 - 20*TESTING), dtype=int)
 
 for line in input_lines:
     regex = r'Step ([A-Z]) must be finished before step ([A-Z]) can begin.'
     needed, todo = re.match(regex, line).groups()
-    requirements[index(todo), index(needed)] = 60 + index(needed) + 1
+    requirements[index(todo), index(needed)] = 60 * (not TESTING) + index(needed) + 1
+    requirements[-1, index(todo)] = 60 * (not TESTING) + index(todo) + 1
 
-workers = [None, None, None, None, None]
+workers = 2 + 3*(not TESTING)
 
 remaining = steps
-order = []
 time_count = 0
+log_pass = 0
+steps_this_count = []
+
 
 def reqs_done(step):
     return not requirements[index(step)].any()
@@ -59,28 +76,40 @@ def unclaimed(step):
     return step not in workers
 
 
+def progress(step):
+    step_index = index(step)
+    column = requirements[:, step_index]
+    column -= 1
+    column[(column < 0).nonzero()] = 0
+    if not column.any():
+        global remaining
+        remaining = remaining.replace(step, '')
+
+
 while remaining:
+    in_progress = [step for step in steps_this_count if step in remaining]
+    new_steps = [
+        step 
+        for step in remaining 
+        if reqs_done(step) and step not in steps_this_count
+    ]
+    steps_this_count = (in_progress + new_steps)[:workers]
+    if 'log' in sys.argv:
+        if log_pass:
+            log_pass -= 1
+        else:
+            print(f'Time: {time_count}')
+            print(steps_this_count)
+            for step in steps:
+                print(str(step).rjust(3), end='')
+            print('')
+            for row in requirements:
+                for x in row:
+                    print(str(x).rjust(3), end='')
+                print('')
+            log_pass = int(input('') or 1) - 1
+    for step in steps_this_count:
+        progress(step)
     time_count += 1
-    if time_count % 50 == 0:
-        print(time_count)
-    for i, step in enumerate(workers):
-        if step is None:
-            candidates = filter(
-                lambda rem: reqs_done(rem) and unclaimed(rem), 
-                remaining
-            )
-            step = next(candidates, None)
-            workers[i] = step
-            if step is not None:
-                print(f"Worker {i} is starting {step}")
-                order.append(step)
-        if step is not None:
-            step_index = index(step)
-            column = requirements[:, step_index]
-            column -= 1
-            column[(column < 0).nonzero()] = 0
-            if not column.any():
-                workers[i] = None
-                remaining = remaining.replace(step, '')
 
 print(time_count)
