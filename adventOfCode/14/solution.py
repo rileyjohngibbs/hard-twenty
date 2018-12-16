67ea9dbe71372f3d202dc7bfc2c375fe0a0efe55
Riley Gibbs <rileyjohngibbs@gmail.com>
2018-12-15 20:42:48 -0800

    Added Day Fourteen, Part Two solution in Python

--- adventOfCode/14/solution.py
+++ adventOfCode/14/solution.py
@@ -2,14 +2,36 @@ import sys
 
 
 def main():
-    recipe_count = digest_input()
-    recipes = [3, 7]
-    elves = [0, 1]
-    while len(recipes) < recipe_count + 10:
+    # Part One
+    if not TESTING or 'one' in sys.argv:
+        recipe_count = int(digest_input())
+        recipes = ['3', '7']
+        elves = [0, 1]
+        while len(recipes) < recipe_count + 10:
+            if TESTING:
+                show(recipes, elves)
+            recipes, elves = iterate(recipes, elves)
         if TESTING:
             show(recipes, elves)
-        recipes, elves = iterate(recipes, elves)
-    print(''.join(map(str, recipes[-10:])))
+        print(''.join(map(str, recipes[recipe_count:recipe_count+10])))
+    # Part Two
+    if not TESTING or 'two' in sys.argv:
+        recipe_list = digest_input()
+        list_length = len(recipe_list)
+        recipes = ['3', '7']
+        elves = [0, 1]
+        last_check = 0
+        while recipe_list not in ''.join(recipes[-list_length-1:]):
+            if len(recipes) - last_check > 1_000_000:
+                last_check = len(recipes) - (len(recipes) % 1_000_000)
+                print(f'Running length of the recipe list: {last_check}')
+            if TESTING:
+                show(recipes, elves)
+            recipes, elves = iterate(recipes, elves)
+        if TESTING:
+            show(recipes, elves)
+        print('Solution:')
+        print(''.join(map(str, recipes)).find(recipe_list))
 
 
 def digest_input():
@@ -18,8 +40,8 @@ def digest_input():
     else:
         filename = 'input.txt'
         with open(filename, 'r') as inputfile:
-            text = inputfile.read()
-        return int(text)
+            text = inputfile.read().replace('\n', '')
+        return text
 
 
 def show(recipes, elves):
@@ -32,22 +54,20 @@ def show(recipes, elves):
 
 
 def iterate(recipes, elves):
-    current_total = sum(recipes[elf] for elf in elves)
-    added_recipes = [int(a) for a in str(current_total)]
-    new_recipes = recipes + added_recipes
-    new_elves = [next_recipe_index(elf, new_recipes) for elf in elves]
-    return new_recipes, new_elves
+    current_total = sum(int(recipes[elf]) for elf in elves)
+    added_recipes = str(current_total)
+    recipes.extend(added_recipes)
+    new_elves = [next_recipe_index(elf, recipes) for elf in elves]
+    return recipes, new_elves
 
 
 def next_recipe_index(current, recipes):
-    current_score = recipes[current]
+    current_score = int(recipes[current])
     new_index = (current + 1 + current_score) % len(recipes)
-    if TESTING:
-        print(f'{current_score} moved to index {new_index}')
     return new_index
 
 
-TESTING = int(next(iter(sys.argv[1:]), '0'))
+TESTING = next(iter(sys.argv[1:]), False)
 
 if __name__ == '__main__':
     main()
