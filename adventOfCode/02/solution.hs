e7cdc6504bf00a8c6fed695a3ce51ea50ea3e35c
Riley Gibbs <rileyjohngibbs@gmail.com>
2018-12-22 14:43:36 -0600

    Added Day Two, Part Two solution in Haskell

--- adventOfCode/02/solution.hs
+++ adventOfCode/02/solution.hs
@@ -3,9 +3,11 @@ import qualified Data.Map.Strict as Map
 main :: IO ()
 main = do
   input <- readFile "input.txt"
-  let counts = map getCounts $ lines input
   -- Part One
+  let counts = map getCounts $ lines input
   (putStrLn . show) $ (nCounts 2 counts) * (nCounts 3 counts)
+  -- Part Two
+  putStrLn $ tossDiff "" $ (findSimilar . lines) input
 
 increment :: Map.Map Char Integer -> Char -> Map.Map Char Integer
 increment m c = Map.alter maybeAdd c m
@@ -26,3 +28,29 @@ countTrue bs = foldl inc 0 bs
 
 nCounts :: Integer -> [Map.Map Char Integer] -> Integer
 nCounts n ms = countTrue $ map (hasCount n) ms
+
+countDiff :: String -> String -> Integer -> Integer
+countDiff (a:as) (b:bs) n = if a /= b
+  then countDiff as bs (n + 1)
+  else countDiff as bs n
+countDiff _ _ n = n
+
+findSimilarHelper :: String -> [String] -> Maybe [String]
+findSimilarHelper a (b:ss) = if countDiff a b 0 == 1
+  then Just [a, b]
+  else findSimilarHelper a ss
+findSimilarHelper a ss = Nothing
+
+findSimilar :: [String] -> Maybe [String]
+findSimilar (a:ss) = if similars == Nothing
+  then findSimilar ss
+  else similars
+  where
+    similars = findSimilarHelper a ss
+findSimilar (ss) = Nothing
+
+tossDiff :: String -> Maybe [String] -> String
+tossDiff s (Just ((a:as):(b:bs):ss)) = if a == b
+  then tossDiff (s ++ [a]) (Just [as, bs])
+  else tossDiff s (Just [as, bs])
+tossDiff s (Just (as:bs:ss)) = s
